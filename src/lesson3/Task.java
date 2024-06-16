24aeb89ee2ba88906fd7d7cf1db5f229f7e17e51
Raman <rdubouski@gmail.com>
2024-06-16 20:09:39 +0300

    Lesson 3 goodbye friend

--- src/lesson3/Task.java
+++ src/lesson3/Task.java
@@ -5,5 +5,6 @@ public class Task {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello friend");
+        System.out.println("Goodbye friend");
     }
 }
